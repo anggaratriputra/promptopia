04b746962dc3cd1dbb1ed98a6b59077dd49c9f30
anggaratriputra <anggaratriputra@gmail.com>
2024-02-22 11:48:44 +0700

    change

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,15 +1,14 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-
-import User from "@models/user";
-import { connectToDB } from "@utils/database";
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import User from '@models/user';
+import { connectToDB } from '@utils/database';
 
 const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
+    })
   ],
   callbacks: {
     async session({ session }) {
@@ -30,18 +29,18 @@ const handler = NextAuth({
         if (!userExists) {
           await User.create({
             email: profile.email,
-            username: profile.name.replace(/\s/g, "").toLowerCase(), // Replace all spaces with an empty string
+            username: profile.name.replace(" ", "").toLowerCase(),
             image: profile.picture,
           });
         }
 
-        return true;
+        return true
       } catch (error) {
         console.log("Error checking if user exists: ", error.message);
-        return false;
+        return false
       }
     },
-  },
-});
+  }
+})
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
